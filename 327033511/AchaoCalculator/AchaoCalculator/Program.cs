2a78d7f870e152368bdb7bb4b68560f1c7ea07d7
327033511 <327033511@qq.com>
2019-09-20 13:18:55 +0800

    AchaoCalculator

--- 327033511/AchaoCalculator/AchaoCalculator/Program.cs
+++ 327033511/AchaoCalculator/AchaoCalculator/Program.cs
@@ -8,78 +8,79 @@ using System.IO;
 
 namespace ConsoleApp1
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            Console.WriteLine("输入需要产生的等式个数：");
             int n = Convert.ToInt32(Console.ReadLine());
-            Formula formula1 = new Formula(n);
-            formula1.MakeFormula(n);
+            Formula formula1 = new Formula();
+            for (int i = 0; i < n; i++)
+            {
+                formula1.MakeFormula();
+                formula1.Print();
+            }
+            Console.WriteLine("所有的等式已保存到指定文件夹！");
             Console.Read();
         }
     }
 
-    class Formula
+    public class Formula
     {
-        int n, num, strnum;
-        string str;
-        string equation = "";
+        DataTable dt = new DataTable();
+        public int strnum;
+        public string str;
+        public string equation = "";
         string[] symbol = { "+", "-", "*", "/" };
-        public Formula(int a)
-        {
-            this.n = a;
-        }
-        public void MakeSym()//制造符号
+        object sum = null;
+        public string MakeSym()//制造符号
         {
             Random ran0 = new Random(Guid.NewGuid().GetHashCode());
-            str = symbol[ran0.Next(0, 4)];
+            return symbol[ran0.Next(0, 4)];
         }
-        public void MakeNum()//制造数字
+        public int MakeNum()//制造数字
         {
             Random ran1 = new Random(Guid.NewGuid().GetHashCode());
-            num = ran1.Next(0, 101);
+            return ran1.Next(0, 101);
 
         }
-        public void MakeFormula(int n)//制造等式
+        public string MakeFormula()//制造等式
         {
-            int i, j = 0;
-            object sum = null;
-            DataTable dt = new DataTable();
-            for (i = 0; i < n; i++)//等式个数
+            start: int j = 0;
+            equation = "";
+            Random ran2 = new Random(Guid.NewGuid().GetHashCode());
+            strnum = ran2.Next(2, 4);//等式符号个数
+            do
             {
-                j = 0;
-                equation = "";
-                Random ran2 = new Random(Guid.NewGuid().GetHashCode());
-                strnum = ran2.Next(2, 4);//等式符号个数
-                do
-                {
-                    MakeNum();
-                    equation += Convert.ToString(num);
-                    MakeSym();
-                    equation += str;
-                    j++;
-                } while (j < strnum);
-                MakeNum();
-                equation += Convert.ToString(num);//等式左边的制造    
+                equation += Convert.ToString(MakeNum());
+                equation += MakeSym();
+                j++;
+            } while (j < strnum);
 
-                sum = dt.Compute(equation, "");//等式右边的制造(计算结果)
+            MakeNum();
+            equation += Convert.ToString(MakeNum());//等式左边的制造    
+            sum = dt.Compute(equation, "");//等式右边的制造(计算结果)
+            equation = equation + "=" + Convert.ToString(sum); //等式的制造
 
-                if (Convert.ToString(sum).Contains(".") || Convert.ToString(sum).Contains("-") || equation.Contains("/0"))
-                {
-                    i--;
-                }//去除不和规则的等式
-                else
-                {
-                    StreamWriter sw = new StreamWriter(@"E:\result\resultprint.txt", true);
-                    sw.WriteLine(equation + "=" + Convert.ToString(sum));
-                    sw.Flush();
-                    sw.Close();
-                }//写入文件
-
-            }
-
+            if (Convert.ToString(sum).Contains(".") || Convert.ToString(sum).Contains("-") || equation.Contains("/0"))
+            {
+                goto start;
+            }//去除不和规则的等式
+            return equation;
         }
 
+        public int Sum()//返回sum的值
+        {
+            return Convert.ToInt32(sum);
+        }
 
+        public void Print() //写入文件、输出
+        {
+            StreamWriter sw = new StreamWriter(@"E:\result\resultprint2.txt", true);
+            sw.WriteLine(equation);
+            Console.WriteLine(equation);
+            sw.Flush();
+            sw.Close();
+        }
     }
 }
